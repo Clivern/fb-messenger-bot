d593883ef003cc47c632c84f4a965f8fc73c3697
A. F <hello@clivern.com>
2017-06-29 17:26:11 +0200

    add cred

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+PAGE_ACCESS_TOKEN = "EAAEPZCEjGRwwBAPcQlB6qv4d356mrMgZBGSNpQGYkG9zZCND86AGzK9n8WqmnMysAjlY2XZCbEbqmuj93yoX3PyzORRH53JzAd7qAGVjMOU6bd08fZAgAnapJz8EBPN5pkpfRnjGul6mDf6FyYllZAvZCo8kYk7mgfGdobyCc8y4MkmdmOv3Nek"
+VERIFY_TOKEN = "djhwndgjxtyewfwgde"
 
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -58,7 +60,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        "access_token": PAGE_ACCESS_TOKEN
     }
     headers = {
         "Content-Type": "application/json"
